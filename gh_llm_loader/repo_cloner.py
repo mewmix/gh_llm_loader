4f32ba5e7d707decdb19656ac89f09b56d62b870
Alex Klein <alex@facings.io>
2024-02-26 21:17:54 -0800

    Update

--- gh_llm_loader/repo_cloner.py
+++ gh_llm_loader/repo_cloner.py
@@ -30,9 +30,9 @@ def clone_and_prepare_repo(git_url, base_dir="./cloned_repos", ignored_folders=N
     
     print(f"Repository cloned and data prepared at {os.path.join(base_dir, output_filename)}")
 
-# Example usage
-git_url = "https://github.com/mewmix/gh_llm_loader.git"  # URL of the GitHub repository
-ignored_folders = {'ignored_folder_name'}  # Optionally specify folders to ignore
-ignored_files = {'ignored_file.txt'}  # Optionally specify files to ignore
+# # Example usage
+# git_url = "https://github.com/mewmix/gh_llm_loader.git"  # URL of the GitHub repository
+# ignored_folders = {'.git'}  # Optionally specify folders to ignore
+# ignored_files = {''}  # Optionally specify files to ignore
 
-clone_and_prepare_repo(git_url, ignored_folders=ignored_folders, ignored_files=ignored_files)
+# clone_and_prepare_repo(git_url, ignored_folders=ignored_folders, ignored_files=ignored_files)
